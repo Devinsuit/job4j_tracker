fd589f6eb9dd289a02c47feae8eb0474a941b2f0
devinsuit <re.markbelfort@gmail.com>
2021-02-10 12:03:18 +0300

    fix professions

--- src/main/java/ru/job4j/oop/inheritance/Engineer.java
+++ src/main/java/ru/job4j/oop/inheritance/Engineer.java
@@ -1,7 +1,6 @@
 package ru.job4j.oop.inheritance;
 
 public class Engineer extends Profession {
-    private String education;
 
     public Engineer(String name, String surname, String education, String birthday) {
         super(name, surname, education, birthday);
--- src/main/java/ru/job4j/oop/inheritance/Programmer.java
+++ src/main/java/ru/job4j/oop/inheritance/Programmer.java
@@ -3,7 +3,9 @@ package ru.job4j.oop.inheritance;
 public class Programmer extends Engineer {
     private String language;
 
-    public Programmer(String name, String surname, String education, String birthday) {
+    public Programmer(String name, String surname, String education, String birthday,
+                      String language) {
         super(name, surname, education, birthday);
+        this.language = language;
     }
 }
