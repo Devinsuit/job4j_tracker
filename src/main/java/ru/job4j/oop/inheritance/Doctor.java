039f7aef111745916228f4650881c1874fa6cc16
devinsuit <re.markbelfort@gmail.com>
2021-02-10 11:21:16 +0300

    fix professions

--- src/main/java/ru/job4j/oop/inheritance/Doctor.java
+++ src/main/java/ru/job4j/oop/inheritance/Doctor.java
@@ -4,8 +4,11 @@ public class Doctor extends Profession {
     private boolean residency;
     private int experience;
 
-    public Doctor(String name, String surname, String education, String birthday) {
+    public Doctor(String name, String surname, String education, String birthday,
+                  boolean residency, int experience) {
         super(name, surname, education, birthday);
+        this.residency = residency;
+        this.experience = experience;
     }
 
     public boolean breakTaken() {
