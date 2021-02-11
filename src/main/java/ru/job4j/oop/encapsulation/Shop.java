69582cc6377ddc4c439e267f5a34e2a4f5e84b31
devinsuit <re.markbelfort@gmail.com>
2021-02-11 14:32:30 +0300

    fix

--- src/main/java/ru/job4j/oop/encapsulation/Shop.java
+++ src/main/java/ru/job4j/oop/encapsulation/Shop.java
@@ -2,14 +2,12 @@ package ru.job4j.oop.encapsulation;
 
 public class Shop {
     public static int indexOfNull(Product[] products) {
-        int number = 0;
+        int number = -1;
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (product == null) {
                 number = i;
                 break;
-            } else {
-                number = -1;
             }
         }
     return number;
