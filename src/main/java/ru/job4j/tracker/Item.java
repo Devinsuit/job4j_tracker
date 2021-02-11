6b932b9f7cd3ae8e6b30b01986a86bffa23dd40b
devinsuit <re.markbelfort@gmail.com>
2021-02-11 17:50:50 +0300

    5. Tracker - хранилище [#465360]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,27 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created = LocalDateTime.now();
-
-    public Item() {
-    }
-
-    public Item(int number) {
-        this.id = number;
-    }
-
-    public Item(String name, int number) {
-        this.name = name;
-        this.id = number;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
 
     public int getId() {
         return id;
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,10 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
+        Tracker tracker = new Tracker();
         Item item = new Item();
-        LocalDateTime created = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String createdFormat = created.format(formatter);
-        System.out.println("Текущие дата и время: " + createdFormat);
+        tracker.add(item);
+        System.out.println(tracker.findById(item.getId()));
     }
 }
